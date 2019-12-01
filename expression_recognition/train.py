61088a4d8b1a372353d8e7648844898c35f34fcf
Vidit Mohaniya <vimohan@iu.edu>
2019-12-01 01:43:55 -0500

    Updated 5CV

--- expression_recognition/train.py
+++ expression_recognition/train.py
@@ -1,20 +1,13 @@
 import cv2
 import numpy
 from utils import helper as helper
-# import sys
-# from PIL import Image, ImageOps
 
 haar_file = '..\\haar_classifiers\\haarcascade_frontalface_default.xml'
 haar_file_side = '..\\haar_classifiers\\haarcascade_profileface.xml'
 fn_dir = 'database'
 
-# Part 1: Training model
-print('Training starts please wait....')
-images, labels = helper.generate_dataset(fn_dir)
 # Create a list of images and a list of corresponding names
-
-(im_width, im_height) = (112, 92)
-# (im_width, im_height) = (48, 48)
+images, labels = helper.generate_dataset(fn_dir)
 
 # Create array from the two lists above
 (images, labels) = [numpy.array(lis) for lis in [images, labels]]
--- face_recognition/train.py
+++ face_recognition/train.py
@@ -1,23 +1,13 @@
 import cv2
 import numpy
 from utils import helper as helper
-# import sys
-# import os
-# from PIL import Image, ImageOps
 
 haar_file = '..\\haar_classifiers\\haarcascade_frontalface_default.xml'
 haar_file_side = '..\\haar_classifiers\\haarcascade_profileface.xml'
-datasets = 'database'
 fn_dir = 'database'
-
-# Part 1: Training model
-print('Training starts please wait....')
 
 # Create a list of images and a list of corresponding names
 images, labels = helper.generate_dataset(fn_dir)
-
-(im_width, im_height) = (112, 92)
-# (im_width, im_height) = (48, 48)
 
 # Create array from the two lists above
 (images, labels) = [numpy.array(lis) for lis in [images, labels]]
